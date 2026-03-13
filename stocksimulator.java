370fb8a325175b73c7ad500ddffa8bd5e90f8073
SuhaniSharma072 <suhanisharma072@gmail.com>
2026-03-12 23:17:33 -0500

    initial implementation of stock simulator with basic structure and variables

---
+++ stocksimulator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class stocksimulator{
+    public static void main(String [] args){
+        Scanner kb= new Scanner(System.in);
+        Random rand= new Random();
+        // this is what user starts with the amount of money
+        double balance=10000;
+        String [] stocks= {"AAPL","TSLA","AMZN"};
+        double prices= {100.0,200.0,180.0};
+        int[] shares = {0,0,0};
+
+    }
+}     
